a3c29bf91ee80bf2ec4383e5a7db880588be6cdf
Cbachelot <clark.bachelot@alliedelec.com>
2019-11-25 15:09:41 -0600

    updated manifest

---
+++ ds42/vue.config.js
@@ -0,0 +1,13 @@
+// Inside vue.config.js
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'Design System 42',
+      themeColor: '#001d2c',
+      msTileColor: '#001d2c',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+    
+      
+    }
+  }
