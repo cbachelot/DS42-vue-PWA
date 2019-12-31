0fe98b87da99547490519563f2c1e2edcd26b65a
bazartrance <bazarsoundz@gmail.com>
2019-12-31 10:25:57 -0600

    remove spacing

--- ds42/src/components/Card.vue
+++ ds42/src/components/Card.vue
@@ -2,7 +2,7 @@
                     <div>
 <!--<a :href="data.router" v-for="(data,index) in componentslist" :key="index">-->
 <router-link :to="{path:data.router}" v-for="(data,index) in componentslist" :key="index" >
-            <div class="overview-section-cta">
+            <div class="overview-section-cta" >
             <img :src="data.image" :alt="data.altTag">
             <h4>{{data.compTitle}}</h4>
             <p class="overview-section-desc">{{data.shortDesc}}</p>
