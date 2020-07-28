5dc7973ba6626bc9d0312718b915d566a8358941
bazartrance <bazarsoundz@gmail.com>
2020-07-28 16:20:36 -0500

    added some compressed SVGs and Optimizely

--- ds42/src/main.js
+++ ds42/src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import store from './store';
 import VueAnalytics from 'vue-analytics';
 import VueProgressBar from 'vue-progressbar';
+import LoadScript from 'vue-plugin-load-script';
 
 
 Vue.config.productionTip = false
@@ -14,6 +15,10 @@ Vue.use(VueAnalytics, {
   id: 'UA-150754568-1',
   router
 });
+
+Vue.use(LoadScript);
+
+Vue.loadScript("https://cdn.optimizely.com/js/18152340406.js")
 
 new Vue({
   router,
